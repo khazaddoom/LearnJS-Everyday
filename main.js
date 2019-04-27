b456f5972b422d1d5837ce7aa5f5cf7eabfa5a2f
unknown <31121986g@gmail.com>
2019-04-27 20:45:08 +0530

    IIFE

--- main.js
+++ main.js
@@ -273,4 +273,20 @@
 
 // developerQuestion('Ganesh')
 
+// function game() {
+
+//   var score = Math.random() * 10;
+//   console.log(score >= 5)
+
+// }
+
+// game();
+
+
+(function() {
+  var score = Math.random() * 10;
+  console.log(score >= 5)
+})();
+
+console.log(score)
 
