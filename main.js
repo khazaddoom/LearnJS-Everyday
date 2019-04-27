e5dc50af0a345e2144a4cad0a63e793b0194bfee
unknown <31121986g@gmail.com>
2019-04-27 20:46:18 +0530

    IIFE.2

--- main.js
+++ main.js
@@ -283,10 +283,10 @@
 // game();
 
 
-(function() {
+(function(goodLuck) {
   var score = Math.random() * 10;
-  console.log(score >= 5)
-})();
+  console.log(score >= 5 - goodLuck)
+})(5);
 
-console.log(score)
+
 
