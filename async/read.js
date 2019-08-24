b6505e4ba725216c45cc5fd544931779c9c06c81
khazadoom <31121986g@gmail.com>
2019-08-24 07:34:50 +0530

    understand rxjs subject vs promise

--- async/read.js
+++ async/read.js
@@ -3,8 +3,11 @@ let fs = require('fs');
 function readFile() {
     console.log('reading...')
     fs.readFile('./async/data.txt', 'utf-8', function(err, data) {
-        if(err) { console.log('something went wrong', err) ; return; }
-        console.log(data)
+        setInterval(() => {
+            if(err) { console.log('something went wrong', err) ; return; }
+            console.log(data)
+        }, 1000);
+        
     })
 };
 
