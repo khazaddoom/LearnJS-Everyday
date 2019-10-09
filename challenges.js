65290875a5596abe4dc3e697afc67383af38c886
khazaddoom <31121986g@gmail.com>
2019-10-09 19:30:18 +0530

    summing in regular way

---
+++ challenges.js
@@ -0,0 +1,17 @@
+// sum
+
+function sum(...arguments) {
+    // let sum = 0;
+    // Array.from(arguments).forEach( element => {
+    //     sum += element;
+    // })
+
+    // console.log(sum)
+
+    arguments.forEach( element => console.log(element))
+
+}
+
+sum(1,2);
+sum(1,2,3,4,5,6,7,8,9);
+sum(1)
