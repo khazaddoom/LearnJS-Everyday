94cf47e4090259b84970b1b4e26e1a6b01a08c04
khazaddoom <31121986g@gmail.com>
2019-11-01 20:10:19 +0530

    added data to sample project

--- timesheet.js
+++ timesheet.js
@@ -1,5 +1,42 @@
 let currentTimeSheet = [];
+const projects = [
+    { projectId: 101, name: 'SUGi', active: true },
+    { projectId: 102, name: 'EPL world', active: true },
+]
 
+let tasks = [
+    { taskId: 1001, name: 'SUGi Task 1', projectId: 101 },
+    { taskId: 1002, name: 'SUGi Task 2', projectId: 101 },
+    { taskId: 1003, name: 'SUGi Task 3', projectId: 101 },
+    { taskId: 1004, name: 'SUGi Task 4', projectId: 101 },
+    { taskId: 1005, name: 'EPL world Task 1', projectId: 102 },
+    { taskId: 1006, name: 'EPL world Task 2', projectId: 102 },
+    { taskId: 1007, name: 'EPL world Task 3', projectId: 102 },
+    { taskId: 1008, name: 'EPL world Task 4', projectId: 102 },
+]
+
+let users = [
+    {
+        userId: 9001, name: 'ganesh.a', email: 'ganesh.a@juegostudio.com',
+        assignedProjects: [101, 102]
+    },
+    {
+        userId: 9002, name: 'ashwath.an', email: 'ashwath.an@juegostudio.com',
+        assignedProjects: [101, 102]
+    },
+    {
+        userId: 9003, name: 'nayana.shetty', email: 'nayana.shetty@juegostudio.com',
+        assignedProjects: [101, 102]
+    },
+    {
+        userId: 9004, name: 'saurav.kumar', email: 'saurav.kumar@juegostudio.com',
+        assignedProjects: [101, 102]
+    },
+    {
+        userId: 9005, name: 'anusha.karkera', email: 'anusha.karkera@juegostudio.com',
+        assignedProjects: [101, 102]
+    },
+]
 
 function createTimeSheet(date) {
     return {
@@ -26,4 +63,10 @@ function addTasks() {
 
     getSelectedTasks()
 
+}
+
+function showPrjects(user) {
+    user.projects.forEach(element => {
+        projects['projectId'] == element
+    });
 }
