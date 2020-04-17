be2a67d12334f938f5377f9a7f32132f1d49b879
khazaddoom <31121986g@gmail.com>
2020-04-17 09:56:41 +0530

    Test

--- docs/main.js
+++ docs/main.js
@@ -1530,14 +1530,3 @@
 //     console.log(a)
 // }
 
-if (navigator.share) { 
-  navigator.share({
-     title: 'WebShare API Demo',
-     url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
-   }).then(() => {
-     console.log('Thanks for sharing!');
-   })
-   .catch(console.error);
-} else {
-  console.error('Share not available!')
-}
